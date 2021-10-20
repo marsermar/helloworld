d597b02329e75059b4a11ff90175e2fe27dcc759
Mártha-Sersényi Mariann <sersenyim@gmail.com>
2021-10-21 00:08:14 +0200

    Initial commit

---
+++ src/com/greenfoxacademy/print/HelloMe.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.print;
+
+public class HelloMe {
+    public static void main(String[] args) {
+        // Úgy módosítsd a programot, hogy téged üdvözöljön a Világ helyett
+        System.out.println("Hello, Mariann!");
+    }
+}
+
